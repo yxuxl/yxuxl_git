1190966204244c0dc5ba9eb62dda131a97309b09
yxuxl <crystal333999@naver.com>
2024-10-10 14:55:07 +0900

    Add files via upload

--- hw4.c
+++ hw4.c
@@ -1,26 +1,26 @@
-#define CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-	int num;
-	int i;
-	int is_prime = 1;
+	int num=0;
+	int i=0;
+	int prime = 1;
 
 	printf("Please enter a number: ");
 	scanf("%d", &num);
 
 	if (num <= 1)
-		is_prime = 0;
+		prime = 0;
 
 	for (i = 2; i < num; i++)
 	{
 		if (num % i == 0)
-			is_prime = 0;
+			prime = 0;
 		    break;
 
 	}
-	if (is_prime == 1)
+	if (prime == 1)
 		printf("It is a prime number.\n");
 	else
 		printf("It is not a prime number.\n");
